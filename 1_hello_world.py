3d6b16e29a9c25c35d519223271e564671afe4a0
Shoxdevweb <133132659+Shoxdevweb@users.noreply.github.com>
2023-05-10 18:25:42 +0300

    Add files via upload

---
+++ 1_hello_world.py
@@ -0,0 +1,36 @@
+# =============================================================================
+# t_yil = int(input('Tug\'ilgan yilingizni kiriting:  '))
+# yosh = 2023 - t_yil
+# print('Sizning ' , yosh , 'yoshingiz da ekan!')
+# =============================================================================
+# =============================================================================
+# boys = ['Abbos','Shuhrat','Malika']
+# #print('ikkinchi bola:',boys[1] )
+# mevalar = ['olma','anor','uzum','xurmo','shaftoli','girdoli','olcha','behi']
+# moi_druzya = ['Леша','Sasha','Ivan']
+# for moi in moi_druzya:
+#     print('Salom',moi)
+# =============================================================================
+# =============================================================================
+# sonlar = list(range(0,100,10))
+# for son in sonlar:
+#     print(f"{son}  ning kvadrati {son**2} ga teng")
+# =============================================================================
+# =============================================================================
+# bolalar = ['Ali','Vali','husan','Hasan','Olim']
+# for ism in bolalar:
+#     print(f"Salom {ism}")
+# =============================================================================
+# =============================================================================
+# toq_sonlar = list(range(10,100,1))
+# for son in toq_sonlar:
+#     print(f"{son}sonlar kubi {son**3} ga teng")
+# =============================================================================
+# ====================================================================
+dostlar = []
+print("5 ta eng yaxshi do'stingizni kiriting")
+for k in range(5):
+    dostlar.append(input(f"{k+1}-do'stingizni kiriting "))
+    
+    print(dostlar)
+    
---
+++ darslar.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  5 11:19:12 2023
+
+@author: Administrator
+"""
+
+#ismlar = ['shox','malik','behruz','asadbek','oisha']
+"""
+ism = input('Ismingini kiriting!\n ')
+if ism.lower() != 'ali':
+    print(f"Uzr, {ism.title()} Ali ni kutyapmiz!")
+else:
+    print('Salom,Ali')
+"""
+# =============================================================================
+# yosh = int(input('Iltimos,yoshingizni kiriting!    '))
+# if yosh >= 18:
+#     print("Xush kelibsiz!")
+# else:
+#     print('Kirish, mumkin emas!')
+# =============================================================================
+# =============================================================================
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#     if car=='gm':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+# =============================================================================
+# =============================================================================
+# son = float(input("Istalgan son kiriting:>>>>"))
+# if son>0:
+#     print(son**(1/2),)
+# else:
+#     print("Iltimos musbat son kiriting!")
+# =============================================================================
+#11-dars Amaliyot 
+# juft son kiritsa rahmat chiqarsin
+
+son = float(input("Juft son kiriting!  "))
+if son%2==0:
+    print('Rahmat')
+else:
+    print('juft son kiritmadingiz')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
